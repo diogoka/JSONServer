c855b6db3a14c0c9125124780746b9f6a8eeb853
diogoka <diogokalmeida@gmail.com>
2023-01-27 20:24:32 -0800

    initial commit5

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.use(jsonServer.rewriter({
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
   if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
+    // req.body.createdAt = Date.now()
   }
   // Continue to JSON Server router
   next()
