477e16e1a33dd8a168731f5179ed99c09a1b0d74
diogoka <diogokalmeida@gmail.com>
2023-01-27 20:07:14 -0800

    initial commit2

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
