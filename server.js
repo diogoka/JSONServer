ee4d53c6ea33484b4e914bdbf4ab4470cdd63135
diogoka <diogokalmeida@gmail.com>
2023-01-28 17:33:54 -0800

    message

--- server.js
+++ server.js
@@ -6,20 +6,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-
-server.use(jsonServer.bodyParser)
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    // req.body.createdAt = Date.now()
-  }
-  // Continue to JSON Server router
-  next()
-})
-
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
